0f76150efc43cb5784b04509725c6b7488fd609b
tianyingchun <tianyingchun@outlook.com>
2014-07-25 14:00:46 +0800

    updates

--- config/index.js
+++ config/index.js
@@ -7,13 +7,17 @@ var qianbaoCfg = {
         orderRemoteUri: "https://test2-www.1qianbao.com:4443/ffastpay",
         merchantId: "900000000009",
         merchantKey: "dc2eb86cfd1e4134bd2368bc28820adf",
-        cachier: "https://test2-www.1qianbao.com:7443/cashier/{transId}" // cacheier request.
+        cachier: "https://test2-www.1qianbao.com:7443/cashier/{transId}", // cacheier request.
+        backEndUrl: "", //后台通知URL
+        frontEndUrl: "http://192.168.14.145:3000/order/detail/{0}", //前台通知URL
     },
     production: {
-        orderRemoteUri: "https://180.168.41.175/ffastpay", //https://mapi.1qianbao.com/ffastpay",
+        orderRemoteUri: "https://mapi.1qianbao.com/ffastpay",
         merchantId: "900000000256",
         merchantKey: "a85f52756a13402191155ee8a7b3f309",
-        cachier: "https://www.1qianbao.com/cashier/{transId}" // cachier request.
+        cachier: "https://www.1qianbao.com/cashier/{transId}", // cachier request.
+        backEndUrl: "", //后台通知URL
+        frontEndUrl: "http://dev.1qianbao.com/qrcode/order/detail/{0}", //前台通知URL
     }
 };
 qianbaoCfg.staging = qianbaoCfg.local;
@@ -26,8 +30,6 @@ var merchantCfg = {
     orderCurrency: "CNY", // 人民币
     transType: "001", //交易类型-001[消费]  005: 查询
     transCode: "0014", //0001", //交易代码 0001[既时支付],0014:[扫码付]
-    backEndUrl: "", //后台通知URL
-    frontEndUrl: "http://192.168.14.145:3000/order/detail/{0}", //前台通知URL
     sameOrderFlag: "N" // 当前订单是否允许重复
 };
 // site configuration.
@@ -67,7 +69,7 @@ var serverCfg = {
 };
 
 module.exports = function(mode) {
-    var env = 'local';
+    var env = 'production';
     var use = serverCfg[mode || process.argv[2] || env] || serverCfg[env];
     var use = _.extend(use, config, merchantCfg, qianbaoCfg[env]);
     return use;
